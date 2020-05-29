ab9ec5746233436dbfbd19789c15d70edb682708
joserosalesdh <jm.rosales92@gmail.com>
2020-05-29 10:55:52 -0300

    Modifique la carpeta de ruteos como hicimos hace unas semanas

--- site/src/routes/users.js
+++ site/src/routes/users.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
+// ************ Require's ************
+const express = require('express');
+const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// ************ Controller Require ************
+const usersController = require('../controllers/usersController');
+
+router.get('/', usersController.root); /* GET - All Users */
+
+/*** CREATE ONE User ***/ 
+router.get('/create/', usersController.create); /* GET - Form to create */
+router.post('/create/', usersController.store); /* POST - Store in DB */
+
+/*** EDIT ONE User ***/ 
+router.get('/edit/:id', usersController.edit); /* GET - Form to create */
+router.put('/edit/:id', usersController.update); /* PUT - Update in DB */
+
+/*** DELETE ONE User***/ 
+router.delete('/delete/:id', usersController.destroy); /* DELETE - Delete from DB */
 
 module.exports = router;
