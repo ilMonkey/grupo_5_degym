9b917a3a0a94600ccfbae0eb1ad00b7ab41aa24b
joserosalesdh <jm.rosales92@gmail.com>
2020-06-02 17:07:39 -0300

    Creé la funcionalidad de todos los metodos a utilizar y agregue una imagen de prueba

--- site/src/controllers/productsController.js
+++ site/src/controllers/productsController.js
@@ -7,38 +7,70 @@ const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 const controller = {
-	// Root - Show all products
-	root: (req, res) => {
-		// Do the magic
-	},
 
-	// Detail - Detail from one product
+	// Detail - Metodo que detalla la clase seleccionada
 	detail: (req, res) => {
-		// Do the magic
+		let idDelProducto= req.params.id;
+		let product = products.find(product => product.id == idDelProducto)
+		let finalPrice = (product.price) * (1-(product.discount/100))
+		res.render('detail', {product, finalPrice, toThousand})
 	},
 
-	// Create - Form to create
+	// Create - Este metodo viaja por GET cuando tocamos Crear Nuevo Producto
 	create: (req, res) => {
-		// Do the magic
+		res.render('product-create-form')
 	},
 	
-	// Create -  Method to store
+	// Create -  Metodo que viaja por POST para crear un producto
 	store: (req, res) => {
-		// Do the magic
+		let nuevoProducto= {
+			id: products.length + 1,
+			name: req.body.name,
+			price: req.body.price,
+			discount: req.body.discount,
+			category: req.body.category,
+			description: req.body.description,
+			image: "imagen_de_prueba.jpg",
+		}
+		let nuevaBaseDD = [...products, nuevoProducto];
+		let nuevaBaseDDJSON = JSON.stringify(nuevaBaseDD);
+		fs.writeFileSync(productsFilePath, nuevaBaseDDJSON);
+		res.redirect('/')
 	},
 
-	// Update - Form to edit
+	// Update - Metodo que viaja por GET para editar un producto
 	edit: (req, res) => {
-		// Do the magic
+		let id= req.params.id;
+		let product = products.find(product => product.id == req.params.id)
+		res.render('product-edit-form', {product})
 	},
-	// Update - Method to update
+	// Update - metodo que viaja por POST cuando ya realizamos todos las ediciones necesarias
 	update: (req, res) => {
-		// Do the magic
+		let editId = req.params.id
+        products.forEach(product => {
+        if (product.id == editId) {
+            product.name = req.body.name
+            product.description = req.body.description
+            product.price = req.body.price
+            product.discount = req.body.discount
+            product.image = "imagen_de_prueba-jpg"
+            product.category = req.body.category           
+        }            
+        });
+        let productsJson = JSON.stringify(products)
+        fs.writeFileSync(productsFilePath, productsJson)
+        res.redirect('/')
 	},
 
-	// Delete - Delete one product from DB
+	// Delete - Metodo que viaja por DELETE para borrar un producto
 	destroy : (req, res) => {
-		// Do the magic
+		let idDelProductoABorrar = req.params.id;
+		let nuevaBaseDeDatos = products.filter((product) => product.id != idDelProductoABorrar  ) 
+		// (product) representa a cada una de las posiciones del array, va recorriendo una por una
+		let nuevaBaseDeDatosJSON = JSON.stringify(nuevaBaseDeDatos);
+		fs.writeFileSync(productsFilePath, nuevaBaseDeDatosJSON)
+		// primer parametro el path, segundo que le meto a ese path
+		res.redirect('/')
 	}
 };
 
--- site/src/routes/products.js
+++ site/src/routes/products.js
@@ -5,18 +5,18 @@ const router = express.Router();
 // ************ Controller Require ************
 const productsController = require('../controllers/productsController');
 
-router.get('/', productsController.root); /* GET - All products */
-router.get('/detail/:id', productsController.detail); /* GET - Product detail */
+// router.get('/', productsController.root); /* GET - All products */
+router.get('/:id', productsController.detail); /* GET - Product detail */
 
 /*** CREATE ONE PRODUCT ***/ 
 router.get('/create/', productsController.create); /* GET - Form to create */
-router.post('/create/', productsController.store); /* POST - Store in DB */
+router.post('/', productsController.store); /* POST - Store in DB */
 
 /*** EDIT ONE PRODUCT ***/ 
-router.get('/edit/:id', productsController.edit); /* GET - Form to create */
-router.put('/edit/:id', productsController.update); /* PUT - Update in DB */
+router.get('/:id/edit', productsController.edit); /* GET - Form to create */
+router.put('/:id', productsController.update); /* PUT - Update in DB */
 
 /*** DELETE ONE PRODUCT***/ 
-router.delete('/delete/:id', productsController.destroy); /* DELETE - Delete from DB */
+router.delete('/:id', productsController.destroy); /* DELETE - Delete from DB */
 
 module.exports = router;
