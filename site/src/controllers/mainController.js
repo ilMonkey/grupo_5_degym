1e3ae60e8559193e8bb2e163f5d27acdf633fc66
hastux <hastux@yahoo.com>
2020-06-13 18:52:03 -0300

    Agregué las sedes desde el JSON en el footer

--- site/src/controllers/mainController.js
+++ site/src/controllers/mainController.js
@@ -1,14 +1,21 @@
 const fs = require('fs');
 const path = require('path');
 
+// DataBase Products
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+
+// DataBase Branches
+const branchesFilePath = path.join(__dirname, '../data/branchesDataBase.json');
+var branches = JSON.parse(fs.readFileSync(branchesFilePath, 'utf-8'));
+branches = branches.filter(branch => branch.id < 5 );
 
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 const controller = {
 	root: (req, res) => {
-		res.render('index', {products})
+		res.render('index', {products, branches})
 	},
 	search: (req, res) => {
 		// Do the magic
