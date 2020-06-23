4eb82eac502eb75ec8c2cdaf3866129a998f6c16
ilmonkey <maritopsi@gmail.com>
2020-06-23 18:16:46 -0300

    mas del prompt

---
+++ public/js/deletePrompt.js
@@ -0,0 +1,22 @@
+/* script para confirmar si usaurio quiere eliminar su perfil */
+
+let deleteForm = document.querySelector('deleteForm')
+
+deleteForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+        swal({
+            title: "Estas seguro de borrar tu Perfil?",
+            text: "Una vez eliminado, perderás todos tus datos y actividades ya abonadas!",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+    })
+    .then((willDelete) => {
+        if (willDelete) {
+            swal("Listo! Tu Usuario ya no pertenece a Da Gym!", {icon: "sad"})
+            this.unbind('submit').submit();
+        } else {
+            swal("Todavía estás inscripto con nosotros.", {icon: "success"});
+        }
+    });
+});
