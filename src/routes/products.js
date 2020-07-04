9e1481fe777e82e73792c5749aae4fe57f41d1c4
joserosalesdh <jm.rosales92@gmail.com>
2020-07-04 12:34:04 -0300

    Solucione un par de errores con el auth middleware en products

--- src/routes/products.js
+++ src/routes/products.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer')
 const path = require('path')
+const authMiddleware = require('../middlewares/authMiddleware');
 
 // ************ Controller Require ************
 const productsController = require('../controllers/productsController');
