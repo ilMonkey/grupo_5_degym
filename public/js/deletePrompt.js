090fe5c9fa894c3688b9872d3c448f6a2e699168
ilmonkey <maritopsi@gmail.com>
2020-07-04 18:47:15 -0300

    cambio en el js deletePrompt

--- public/js/deletePrompt.js
+++ public/js/deletePrompt.js
@@ -1,47 +1,21 @@
 /* script para confirmar si usaurio quiere eliminar su perfil */
 
-// let deleteForm = document.querySelector('#deleteForm')
-$('#deleteForm').on('click', function(e) {
+let deleteForm = document.querySelector('#deleteForm')
+deleteForm.addEventListener('click', function (e) {
     e.preventDefault();
-    var currentElement = $(this);
-
-    swal({
+        swal({
             title: "Estas seguro de borrar tu Perfil?",
             text: "Una vez eliminado, perderás todos tus datos y actividades ya abonadas!",
             icon: "warning",
             buttons: true,
             dangerMode: true,
-            closeModal: "button"
-            })
+    })
     .then(
         (willDelete) => {
         if (willDelete) {
-            $('#deleteForm').trigger('click', { true });
+            $('#deleteForm').trigger('click', {});
         } else {
             swal("Todavía estás inscripto con nosotros.", {icon: "success"});
-        }}
-    );
+        }
+    });
 });
-
-
-
-
-
-// deleteForm.addEventListener('click', function (e) {
-//     e.preventDefault();
-//         swal({
-//             title: "Estas seguro de borrar tu Perfil?",
-//             text: "Una vez eliminado, perderás todos tus datos y actividades ya abonadas!",
-//             icon: "warning",
-//             buttons: true,
-//             dangerMode: true,
-//     })
-//     .then(
-//         (willDelete) => {
-//         if (willDelete) {
-//             $('#deleteForm').trigger('click', {});
-//         } else {
-//             swal("Todavía estás inscripto con nosotros.", {icon: "success"});
-//         }
-//     });
-// });
