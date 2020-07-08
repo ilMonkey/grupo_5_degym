aefb71c9467579f451b4784b04a888b00a5e4e9b
hastux <hastux@yahoo.com>
2020-07-08 16:48:52 -0300

    agregado model activities

---
+++ src/database/models/activities.js
@@ -0,0 +1,14 @@
+module.exports = (sequelize,DataTypes)=> {
+    const activity = sequelize.define(
+        'Activity',
+        {
+            name:DataTypes.STRING,
+            description:DataTypes.STRING,
+            monthly_price:DataTypes.INTEGER,
+        },
+    );
+    activity.associate = (models) => {
+        activity.hasMany(models.Lessons)
+        }
+        return activity
+    }
