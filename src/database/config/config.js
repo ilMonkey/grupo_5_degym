dd5c1742e0e2e2bf3ff3588c5df5444df3ffd313
joserosalesdh <jm.rosales92@gmail.com>
2020-07-08 16:00:18 -0300

    Creé el modelo users.js

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,7 +2,7 @@ module.exports ={
     "development": {
       "username": "root",
       "password": null,
-      "database": "daGym_DB",
+      "database": "daGymDB",
       "host": "127.0.0.1",
       "dialect": "mysql",
       "operatorsAliases": false,
