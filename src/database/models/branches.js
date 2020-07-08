e15fad35d2d6a10082a5ed833831ef4e59260630
ilmonkey <maritopsi@gmail.com>
2020-07-08 16:48:14 -0300

    branches.js

---
+++ src/database/models/branches.js
@@ -0,0 +1,19 @@
+module.exports = (sequelize,DataTypes )=>{
+    const branch = sequelize.define(
+        'Branch',
+        {
+            id: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            name: DataTypes.STRING,
+            address: DataTypes.STRING,
+            telephone: DataTypes.STRING,
+        },
+    );
+    branch.associate = (models) => {
+        branch.hasMany(models.Lessons)
+    }
+    return branch
+};
