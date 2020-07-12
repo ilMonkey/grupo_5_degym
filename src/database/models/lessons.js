335952f679a866a204c23dd1561aa634d9b5ee7d
joserosalesdh <jm.rosales92@gmail.com>
2020-07-12 15:58:10 -0300

    No andan las asociaciones

--- src/database/models/lessons.js
+++ src/database/models/lessons.js
@@ -10,26 +10,16 @@ module.exports = (sequelize,DataTypes )=>{
                 timestamps: false
             },   
     );
-        // lesson.associate = (models)=>{
-            // lesson.belongsTo(models.Activity, {
-            //     as: 'activity',
-            //     foreignKey: 'id_activity',
-            // })
-            // lesson.belongsTo(models.Branch, {
-            //     as: 'branch',
-            //     foreignKey: 'id_branch',
-            // })    
-        // }
-        // lesson.associate = (models)=>{
-        //     Lesson.belongsTo(models.Activity, {
-        //          as: 'activity',
-        //          foreignKey: 'id_activity'
-        //     })
-        //     Lesson.belongsTo(models.Branch, {
-        //         as: 'branch',
-        //         foreignKey: 'id_branch'
-        //     })    
-        // }
+        lesson.associate = (models)=>{
+            Lesson.belongsTo(models.Activity, {
+                 as: 'activity',
+                 foreignKey: 'id_activity'
+            })
+            Lesson.belongsTo(models.Branch, {
+                as: 'branch',
+                foreignKey: 'id_branch'
+            })    
+        }
     
         return lesson
 }
