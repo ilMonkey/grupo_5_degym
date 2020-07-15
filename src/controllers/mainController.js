8c46a8de6046c9d20443b1998164378662e97380
ilmonkey <maritopsi@gmail.com>
2020-07-15 19:00:48 -0300

    main controller fix

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -6,16 +6,26 @@ const productsFilePath = path.join(__dirname, '../data/products.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 // DataBase Branches
-const branchesFilePath = path.join(__dirname, '../data/branches.json');
-var branches = JSON.parse(fs.readFileSync(branchesFilePath, 'utf-8'));
-branches = branches.filter(branch => branch.id < 7 );
+
+
+
+// const branchesFilePath = path.join(__dirname, '../data/branches.json');
+// var branches = JSON.parse(fs.readFileSync(branchesFilePath, 'utf-8'));
+// branches = branches.filter(branch => branch.id < 7 );
 
 
 const mainController = {
-	root: (req, res) => {
-		let usuario = req.session;
-		res.render('index', {products, branches, usuario})
-	}
-};
+	root: async (req, res) => {
+		try {	
+			let usuario = req.session;
+			let products = await DB.Lesson.findAll()
+			let activities = await DB.Activity.findAll()
+			let branches = await DB.Branch.findAll()
+		res.render('index', {products, avtivities, branches, usuario})
+		} catch (error) {
+			res.send(error)
+			}
+		}
+	};
 
 module.exports = mainController;
